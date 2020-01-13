fdbfffccc9021e217b733c2fa939dda4a63ca691
Silizias <66546362+KittenApps@users.noreply.github.com>
2020-01-13 17:03:13 +0100

    error handling

--- functions/decryptkey.js
+++ functions/decryptkey.js
@@ -17,9 +17,15 @@ exports.handler = (event, context, callback) => {
 
         const decipher = createDecipheriv('aes-256-gcm', k, Buffer.from(iv, 'hex'));
         decipher.setAuthTag(Buffer.from(auth, 'hex'));
-        let decrypted = decipher.update(key, 'hex', 'utf8');
-        decrypted += decipher.final('utf8');
-        console.log(`decrypted key: ${decrypted}`);
-        callback(null, {statusCode: 200, body: JSON.stringify({key: decrypted})});
+
+        try {
+            let decrypted = decipher.update(key, 'hex', 'utf8');
+            decrypted += decipher.final('utf8');
+            console.log(`decrypted key: ${decrypted}`);
+            callback(null, {statusCode: 200, body: JSON.stringify({key: decrypted})});
+        } catch (e) {
+            console.log(`Error: ${e.message}`);
+            callback(null, {statusCode: 500, body: "Invalid authTag!"});
+        }
     });
 }
