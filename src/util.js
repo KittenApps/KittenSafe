170189624effe1f8e9da15269ba9248298567a8e
Silizias <66546362+KittenApps@users.noreply.github.com>
2020-02-26 16:18:14 +0100

    readFileAsBuffer use new Promise-based API where available

--- src/util.js
+++ src/util.js
@@ -2,6 +2,7 @@ import React, { createContext, useState, useEffect } from 'react';
 
 export function readFileAsBuffer(file){
   if (file.markdown) return Promise.resolve(new TextEncoder('utf-8').encode(file.markdown));
+  if (file.arrayBuffer) return file.arrayBuffer().then(b => new Uint8Array(b)); // new Promise Blob API
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.onload = e => resolve(new Uint8Array(e.target.result));
